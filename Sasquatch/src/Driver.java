2a91c53a524a590f69dec3f37e496a9d80c66243
Heather Monteson <hemo7757@colorado.edu>
2024-03-02 11:36:16 -0700

    sass baby!

---
+++ Sasquatch/src/Driver.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+
+public class Driver implements Runnable{
+
+    String threadName;
+    int time;
+    Random rand=new Random();
+
+    @Override
+    public void run() {
+        
+    }
+}
